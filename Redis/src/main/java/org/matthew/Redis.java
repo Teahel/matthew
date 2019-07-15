2900af4c1a030d7ed0a603f2ecd3ab68c17dc705
Teahel <Teahel@163.com>
2019-07-16 01:07:04 +0800

    redis测试学习

--- Redis/src/main/java/org/matthew/Redis.java
+++ Redis/src/main/java/org/matthew/Redis.java
@@ -1,5 +1,7 @@
 package org.matthew;
 
+import redis.clients.jedis.Jedis;
+
 /**
  * Created by Theahel
  * User: Matthew
@@ -8,5 +10,13 @@ package org.matthew;
  * Description: No Description
  */
 public class Redis {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("193.112.65.200", 6379);
+        jedis.set("hello","javaRedis");
+        String hello = jedis.get("hello");
+        System.out.println(hello);
+        jedis.close();
+    }
+
 
 }
