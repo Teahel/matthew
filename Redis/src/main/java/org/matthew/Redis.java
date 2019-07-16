65fe5328ccec7ee0f3c99986161d2b60610be0ac
Teahel <Teahel@163.com>
2019-07-16 23:46:15 +0800

    redis增删查改

--- Redis/src/main/java/org/matthew/Redis.java
+++ Redis/src/main/java/org/matthew/Redis.java
@@ -2,6 +2,8 @@ package org.matthew;
 
 import redis.clients.jedis.Jedis;
 
+import java.util.List;
+
 /**
  * Created by Theahel
  * User: Matthew
@@ -17,7 +19,10 @@ public class Redis {
        // new Redis().findAll();
        // new Redis().findByKey();
        // new Redis().update();
-        new Redis().delete();
+        //new Redis().delete();
+      // new Redis().key();
+       // new Redis().list();
+        new Redis().set();
         /*jedis.set("hello","javaRedis");
         String hello = jedis.get("hello");
         System.out.println(hello);
@@ -44,7 +49,7 @@ public class Redis {
      * 查询某一个key的值
      */
     public void findByKey(){
-        String text = jedis.get("two");
+        String text = jedis.get("one");
         System.out.println(text);
     }
 
@@ -58,4 +63,43 @@ public class Redis {
     public void delete(){
         jedis.del("one");
     }
+
+    /**
+     * 获取key的值
+     */
+    public void key(){
+     System.out.println(jedis.exists("two"));
+    }
+
+    public void list(){
+        // 从头添加
+       jedis.lpush("list", "tom", "jack", "jason", "peter", "lc", "easy");
+
+       // 从尾添加
+       jedis.rpush("list", "andy", "eleven");
+
+      // 返回List长度
+      jedis.llen("list");
+
+      // 取值
+        List<String> list = jedis.lrange("list", 0, -1);
+         for (String string : list) {
+          System.out.println(string);
+         }
+
+
+    }
+
+    //操作Set
+        public void set() {
+            jedis.sadd("city", "北京", "上海", "南京", "武汉", "安徽", "广州", "深圳", "山东");
+            System.out.println("取最上面的值：" + jedis.spop("city"));
+            System.out.println("随机取值" + jedis.srandmember("city"));
+
+            jedis.sadd("city2", "北京", "武汉", "河北", "张家界", "辽林", "云南", "陕西", "山西", "黑龙江");
+            System.out.println("交集：" + jedis.sinter("city", "city1"));
+            System.out.println("并集：" + jedis.sunion("city", "city1"));
+            System.out.println("差集：" + jedis.sdiff("city", "city1"));
+        }
+
 }
