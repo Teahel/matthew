9ffde05d548b532372d55526e167c6069c3be2cf
teahel <teahel@163.com>
2019-11-13 17:59:07 +0800

    博客信息查询添加

--- corestart/src/main/java/com/matthew/controller/WebInfoController.java
+++ corestart/src/main/java/com/matthew/controller/WebInfoController.java
@@ -1,10 +1,22 @@
 package com.matthew.controller;
 
+import com.matthew.Utils.ReturnResultUtils;
+import com.matthew.service.WebInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/webinfo")
 public class WebInfoController {
-    
+    @Autowired
+    private WebInfoService webInfoService;
+
+    @RequestMapping(value = "/find",method = RequestMethod.POST)
+    public ReturnResultUtils findWebInfo(){
+        ReturnResultUtils returnResultUtils = new ReturnResultUtils();
+        returnResultUtils.putObject(webInfoService.findWebInfo());
+        return returnResultUtils;
+    }
 }
--- corestart/src/main/java/com/matthew/service/WebInfoService.java
+++ corestart/src/main/java/com/matthew/service/WebInfoService.java
@@ -4,5 +4,9 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.matthew.entity.WebInfor;
 
 public interface WebInfoService extends IService<WebInfor> {
+   /**
+    * 查询网站信息
+    * @return 网站信息对象
+    */
    WebInfor findWebInfo();
 }
